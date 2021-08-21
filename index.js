c757352676dafee0dc57f772f401a27ff3f5d4a8
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-08-21 10:13:44 +0300

    adding generate transactions at the index.js to show them on the Blockchain page

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
-  });
-  
+});
+
 
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
@@ -111,6 +111,45 @@ const syncWithRootState = () => {
         }
     });
 };
+
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+    const transaction = wallet.createTransaction({
+        recipient, amount, chain: blockchain.chain
+    });
+
+    transactionPool.setTransaction(transaction);
+};
+
+const walletAction = () => generateWalletTransaction({
+    wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+    wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+    wallet: walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+for (let i = 0; i < 20; i++) {
+    if (i % 3 === 0) {
+        walletAction();
+        walletFooAction();
+    } else if (i % 3 === 1) {
+        walletAction();
+        walletBarAction();
+    } else {
+        walletFooAction();
+        walletBarAction();
+    }
+
+    transactionMiner.mineTransactions();
+}
 
 let PEER_PORT;
 
