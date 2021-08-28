02c2b4e2f79434715b2dcd05faf7876c53001170
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-08-28 14:31:18 +0300

    making Ellectify as a link to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,7 +8,7 @@ const Home = (props) => {
     // },4000)
     return (
         <div className="container home">
-            <h4 className="center">Home</h4>
+            {/* <h4 className="center">Home</h4> */}
             <h1 className="center">Wellcome!</h1>
             <h3 className="center">Today is ellection day!</h3>
             {/* <img src={BallotBox} alt="BallotBox" /> */}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
         <nav className="nav-wrapper blue darken-3">
             <div className="container">
-                <Link className="brand-logo">Ellectify</Link>
+                <Link className="brand-logo" to="/Home">Ellectify</Link>
                 <ul className="right">
-                    <li><NavLink to="/Home">Home</NavLink></li>
+                    {/* <li><NavLink to="/Home">Home</NavLink></li> */}
                     <li><NavLink to="About">Vote</NavLink></li>
                     <li><NavLink to="/Contact">Party List</NavLink></li>
                     <li><NavLink to="/BlockApp">Blockchain</NavLink></li>
