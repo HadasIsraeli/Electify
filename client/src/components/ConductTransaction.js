5c8d9b18e1ede95e0eb954570a1ec620c63b8e86
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-08-29 14:40:34 +0300

    changing card color and prop type

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -5,35 +5,40 @@ import { FormGroup, FormControl, Button } from 'react-bootstrap';
 import { withRouter } from "react-router-dom";
 
 function VoteCard() {
-    // const parties = [
-    //     { title: "Party1", description: "party1 description", number: "12" },
-    //     { title: "Party2", description: "party2 description", number: "123" },
-    //     { title: "Party3", description: "party3 description", number: "124" },
-    //     { title: "Party4", description: "party4 description", number: "125" },
-    //     { title: "Party5", description: "party5 description", number: "126" },
-    //   ];
+    const parties = [
+        { title: "Party1", description: "party1 description", number: "11" },
+        { title: "Party2", description: "party2 description", number: "22" },
+        { title: "Party3", description: "party3 description", number: "33" },
+        { title: "Party4", description: "party4 description", number: "44" },
+        { title: "Party5", description: "party5 description", number: "55" },
+      ];
 
     return (
-        <div className="container__card">
-            <Card title="Party1" description="party1 description"/>
-            <Card title="Party2" description="party2 description"/>
-            <Card title="Party3" description="party3 description"/>
-            <Card title="Party4" description="party4 description"/>
-            <Card title="Party5" description="party5 description"/>
+        <Card className="container__card">
+      {parties.map((party) => (
+        <PartyCard key={party.number} title={party.title} description={party.description} number={party.number} />
+      ))}
+    </Card>
+    )
+}
+
+function PartyCard(props) {
+    return (
+        <div className="card">
+            <Card>
+                <h4>{props.title}</h4>
+                <p>{props.description}</p>
+                <h6>{props.number}</h6>
+            </Card>
+            <Button>choose party</Button>
         </div>
     )
 }
 
-function Card(props) {
-    return (
-        <div className="card">
-            <div>
-                <h4>{props.title}</h4>
-                <p>{props.description}</p>
-                <h6>{props.info}</h6>
-            </div>
-            <button>choose party</button>
-        </div>
+function Card(props){
+    const classes = props.className + " card";
+    return(
+        <div className={classes}>{props.children}</div>
     )
 }
 
@@ -96,6 +101,7 @@ class ConductTransaction extends Component {
                 <div>
                     <Button onClick={this.conductTransaction}>Submit</Button>
                 </div>
+                <br/>
                 <VoteCard />
             </div>
         )
