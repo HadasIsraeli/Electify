3b9dcf5f1d91fa1db54561ef1b3eba8c21c37719
HADASISRAELI\hadas <hadas25496@gmail.com>
2021-09-01 13:18:01 +0300

    petching data to count vote

--- client/src/components/CountVotes.js
+++ client/src/components/CountVotes.js
@@ -2,31 +2,52 @@ import React, { Component } from 'react';
 
 
 class CountVotes extends Component {
-    // state = { blocks: [] };
+    // state = {
+    //     partyName: null,
+    //     loading: true
+    // };
+
+    // async componentDidMount() {
+    //     const url = `${document.location.origin}/api/blocks`;
+    //     const fetchPromise = await fetch(url);
+    //     const allBlocks = await fetchPromise.json();
+
+    //     const data = allBlocks[1].data[0].outputMap;
+    //     console.log(data);
+    //     this.setState({ partyName: data, loading: false });
+    // }
+
 
     fetchApiBlocks = (cb) => {
-        var url = "http://localhost:3000/api/blocks";
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-        
-        fetch(url,requestOptions).then(response => response.json()).then(allBlocks => cb(allBlocks))
+        var url = `${document.location.origin}/api/blocks`;
+
+        const fetchPromise = fetch(url);
+        fetchPromise.then(response => {
+            return response.json();
+        }).then(allBlocks => {
+            // console.log(allBlocks);
+            const data = allBlocks.map(allBlocks =>
+                allBlocks.data[0]);
+
+            console.log(data);
+        });
     }
 
 
     render() {
         return (
-            
-            <div className="App" onLoad={this.fetchApiBlocks((data)=> console.log({data}))}>
-                <h2>Election Results</h2>
+            // <div>
+            //     {this.state.loading ? (<div>Loading...</div>) :
+            //         (<div>vote</div>)}
+            // </div>
+
+            <div className = "App" onLoad = { this.fetchApiBlocks() } >
+                    <h2>Election Results</h2>
                 {/* <button onClick={
                     this.fetchApiBlocks((data) => console.log({data}))
                     }>Count Votes</button> */}
-                <h3></h3>
-
-            </div>
-        )
+            </div >
+        );
     }
 }
 
